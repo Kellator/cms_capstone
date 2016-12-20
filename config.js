7f9906d598ab4a35de63ff263648cac77b4fdb75
Kelly Miller <k.macleod21@gmail.com>
2016-12-20 22:14:40 +0000

    heroku setup and mlab added

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       (process.env.NODE_ENV === 'production' ?
+                            'mongodb://<dbuser>:<dbpassword>@ds141078.mlab.com:41078/cms_capstone' :
+                            'mongodb://localhost/cms_capstone-dev');
+exports.PORT = process.env.PORT || 8080;
