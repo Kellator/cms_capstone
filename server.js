ca81887772bd7849740149c7abfecdb480374864
Kelly Miller <k.macleod21@gmail.com>
2016-12-21 00:55:50 +0000

    html added for login page and some navigation for dashboard

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+
+var socket_io = require('socket.io');
+var http = require('http');
+
+var server = http.Server(app);
+// var io = socket_io(server);
+
+
 app.listen(process.env.PORT ||8080);
 //comment added for testing
