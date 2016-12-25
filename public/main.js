f59524ccf7f3b169feee39f2cbacfaf3fbcdd746
Kelly Miller <k.macleod21@gmail.com>
2016-12-25 00:20:57 +0000

    contact section data entry fields added

--- public/main.js
+++ public/main.js
@@ -1,7 +1,89 @@
 //screen for user credentials entry
 //screen for dashboard controls
 //screens for client(new entry & existing): contact, prospect, housing, financials, medical. Comments = cumulative.
-var index = 0;
+var newContactEntryMsg = 
+            "<div id='contact_name'>" +
+                "<form action='' method='post'>" +
+                "<fieldset>" +
+                    "<legend>Contact Name</legend>" +
+                    "<label id='contact_last_name'>Last Name:  </label>" +
+                    "<input type='text' required id='contact_last_name' placeholder='Johnson'>" +
+                    "<label id='contact_first_name'>First Name:  </label>" +
+                    "<input type='text' required id='contact_first_name' placeholder='Milly'>" +
+                    "<label class='enter_button' id='enter_contact_name_button'></label>" +
+                    "<button name='enter_contact_name_button' id='enter_contact_name'>Enter</button>" +
+                "</fieldset>" +
+                "</form>" +
+            "</div>" +
+            "<div id='contact_address'>" +
+                "<form action='' method='post'>" +
+                "<fieldset>" +
+                    "<legend>Contact Address</legend>" +
+                    "<label id='contact_street'>Street:  </label>" +
+                    "<input type='text' id='contact_street' placeholder='123 Prospect St.'>" +
+                    "<label id='contact_city'>City:  </label>" +
+                    "<input type='text' id='contact_city' placeholder='Brockton'>" +
+                    "<label id='contact_state'>State:  </label>" +
+                    "<input type='text' id='contact_state' placeholder='MA'>" +
+                    "<label id='contact_zipcode'>Zip:  </label>" +
+                    "<input type='number' id='contact_zipcode' placeholder='02301'>" +
+                    "<label class='enter_button' id='enter_contact_address_button'></label>" +
+                    "<button name='enter_contact_address_button' id='enter_contact_address'>Enter</button>" +
+                "</fieldset>" +
+                "</form>" +
+            "</div>" +
+            "<div id='contact_phone'>" +
+                "<form action='' method='post'>" +
+                "<fieldset>" +
+                    "<legend>Contact Phone Numbers</legend>" +
+                    "<label id='contact_primary_phone'>Primary Phone:  </label>" +
+                    "<input type='number' maxlength='10' required id='contact_primary_phone' placeholder='508-588-5555'>" +
+                    "<label id='contact_alt_phone'>Alternate Phone:  </label>" +
+                    "<input type='number' maxlength='10' id='contact_alt_phone' placeholder='508-588-5858'>" +
+                    "<label class='enter_button' id='enter_contact_phone_button'></label>" +
+                    "<button name='enter_contact_phone_button' id='enter_contact_phone'>Enter</button>" +
+                "</fieldset>" +
+                "</form>" + 
+            "</div>" +
+            "<div id='contact_email'>" +
+                "<form action='' method='post'>" +
+                "<fieldset>" +
+                    "<legend>Contact Email</legend>" +
+                    "<label id='contact_email'>Email:  </label>" +
+                    "<input type='email' id='contact_email' placeholder='name@email.com'>" +
+                    "<label class='enter_button' id='enter_contact_email_button'></label>" +
+                    "<button name='enter_contact_email_button' id='enter_contact_email'>Enter</button>" +
+                "</fieldset>" +
+                "</form>" +
+            "</div>" +
+            "<div id='contact_addl_details'>" +
+                "<form action='' method='post'>" +
+                "<fieldset>" +
+                    "<legend>Additional Details</legend>" +
+                    "<label id='rel_to_prospect'>Relationship to Prospect</label>" +
+                    "<ul>" +
+                        "<li><input type='radio' value='Self' id=rel_self' checked>Self</> " +
+                        "<li><input type='radio' value='Spouse' id=rel_spouse'>Spouse</> " +
+                        "<li><input type='radio' value='Adult Child' id=rel_child'>Adult Child</> " +
+                        "<li><input type='radio' value='Sibling' id=rel_sibling'>Sibling</> " +
+                        "<li><input type='radio' value='Care Professional' id=rel_careProf'>Care Professional</> " +
+                        "<li><input type='radio' value='Other' id='rel_other'>Other</><br><input type='text' placeholder='other relationship'/>" +
+                    "</ul>" +
+                    "<label id='referra_source'>Referral Source</label>" +
+                    "<ul>" + 
+                        "<li><input type='radio' value='Self' id='ref_self' checked>Self</>" +
+                        "<li><input type='radio' value='APFM' id='ref_apfm'>A Place For Mom</>" +
+                        "<li><input type='radio' value='Word of Mouth' id='ref_wom'>Word of Mouth</><br><input type='text' placeholder='Referred by'/>" +
+                        "<li><input type='radio' value='Health Care Provider' id='ref_hcp'>Health Care Provider</><br><input type='text' placeholder='Referred by'/>" +
+                    "</ul>" +
+                    "<label id='first_contact_date'>Date of First Contact </label>" +
+                    "<input type='date' name='first_contact_date'>" +
+                "</fieldset>" +
+                "</form>" +
+            "</div>"
+            ;
+
+
 //what api return data will look like: /api/client_id 
 var Mock_Client_Data = {
     "client_id": [ 
@@ -148,6 +230,7 @@ var Mock_Client_Data = {
     }]
 };
 
+//function to retrieve client_id data
 function getClientData(callbackFn) {
     setTimeout(function(){ callbackFn(Mock_Client_Data)}, 100);
 }
@@ -155,11 +238,22 @@ function getClientData(callbackFn) {
 //would this function instead need to be multiple functions ?  displayClientDataContact, displayClientDataPropsect, displayClientDataFinancials etc?
 function displayClientData(data) {
     console.log(data.client_id);
-    console.log(index);
-    for (index in data.client_id) {
-        $("#test_div").append(
-            "<p>" + data.client_id[index].text + "</p>");
-    //target area to append information e.g. contacts_block    
+    for (var index in data.client_id) {
+        var returnData = data.client_id[index];
+        //variables separate returned data for API
+        var contactData = returnData.contact;
+        var prospectData = returnData.prospect;
+        var housingData = returnData.housingAssistance;
+        var financialsData = returnData.financials;
+        var medicalData = returnData.medical;
+        $("#contact_block").append(newContactEntryMsg);
+        $("#prospect_block").append();
+        $("#housing_block").append();
+        $("#financials_block").append();
+        $("#medical_block").append();
+        $("#comments_block").append();
+            //"<p>" + contactData.contactName + "</p>");
+        //target area to append information e.g. contacts_block    
     }
 }
 
