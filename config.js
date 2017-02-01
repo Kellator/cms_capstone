bb735dc3bc75636a9b1bebe9ff4fbe14853fdb1b
Kelly Miller <k.macleod21@gmail.com>
2017-02-01 03:09:51 +0000

    added mongo user and pass

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://<dbuser>:<dbpassword>@ds141078.mlab.com:41078/cms_capstone' :
+                            'mongodb://millerALCIS:p1ckl3snch33s3@ds141078.mlab.com:41078/cms_capstone' :
                             'mongodb://localhost/cms_capstone-dev');
 exports.PORT = process.env.PORT || 8080;
 
